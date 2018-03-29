2f18c325581f48a9c9d8b49937d8ff63142f15b9
xXDerpyXx <bottledcanofdoom@gmail.com>
2018-03-29 17:55:25 -0400

    adding stuff for discrod

---
+++ main.js
@@ -0,0 +1,9 @@
+var token = "NDI5MDI4NjA5NjM4OTI0Mjg5.DZ7riw.UefSp6c-YGdcK37FM5i3_i3qvZA";
+var Discord = require("discord.js");
+
+
+const client = new Discord.Client();
+client.on('message', msg => {
+
+});
+client.login(token);
