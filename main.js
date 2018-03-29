6849ff8c08dfb87007294158ceb6ae2add4b9c85
xXDerpyXx <bottledcanofdoom@gmail.com>
2018-03-29 17:57:46 -0400

    a foundation

--- main.js
+++ main.js
@@ -1,8 +1,16 @@
 var token = "NDI5MDI4NjA5NjM4OTI0Mjg5.DZ7riw.UefSp6c-YGdcK37FM5i3_i3qvZA";
-var Discord = require("discord.js");
-
-
+const Discord = require("discord.js");
+const fs = require("fs");
 const client = new Discord.Client();
+var data = require("./data.json");
+
+function save(){
+	var temp = JSON.stringify(data);
+	fs.writeFile("./data.json",temp,function(err) {
+		console.log(err);
+	});
+}
+
 client.on('message', msg => {
 
 });
