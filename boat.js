9da52d739cad64f72cfe20fe8464363c61bd2989
xXDerpyXx <bottledcanofdoom@gmail.com>
2018-03-29 18:03:56 -0400

    boatstats

--- boat.js
+++ boat.js
@@ -9,7 +9,28 @@ module.exports = class boat{
         this.attack = 0; //amount of damage per successful hit
         switch(this.type){
             case "submarine":
-
+                this.hp = 5
+                this.dodge = 5;
+                this.range = 3;
+                this.attack = 1;
+            break;
+            case "convoy":
+                this.hp = 3;
+                this.dodge = 1;
+                this.range = 2;
+                this.attack = 3;
+            break;
+            case "destroyer":
+                this.hp = 10;
+                this.dodge = 0;
+                this.range = 5;
+                this.attack = 2;
+            break;
+            case "cruiser":
+                this.hp = 6;
+                this.dodge = 3;
+                this.range = 7;
+                this.attack = 1;
             break;
         }
     }
